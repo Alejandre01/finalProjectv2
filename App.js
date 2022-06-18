d191e606ba9569008c55578b576dfd829c0b7a2a
Bernardo Alejandre <bernardo.alejandre@gmail.com>
2022-06-18 12:26:03 -0700

    Folders Added

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Please Behavea and Change!!!!!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>hello!!</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
