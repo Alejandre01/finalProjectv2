0fa765e6911541d791d9aabf7388addb8bc10c24
Bernardo Alejandre <bernardo.alejandre@gmail.com>
2022-06-18 13:29:16 -0700

    HomeFile

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './components/screens/Home';
+
 
 const App = () => {
-  return (
-    <View>
-      <Text>hello!!</Text>
-    </View>
-  )
-}
+  const Stack = createNativeStackNavigator();
 
-export default App
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+      
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
